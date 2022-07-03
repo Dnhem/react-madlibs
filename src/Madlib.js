c64fe3e66bb989a6471447d540efac27c03a3cf5
Dana Nhem <dananhem@gmail.com>
2022-07-03 09:15:20 +0200

    fixed rendering bug

--- src/Madlib.js
+++ src/Madlib.js
@@ -5,17 +5,19 @@ import { v4 as uuid } from "uuid";
 const Madlib = () => {
   const [ story, setStory ] = useState({});
   const { noun, nounTwo, adjective, color } = story;
+  const [ madlib, setMadlib ] = useState("");
   const createStory = newStory => {
     setStory(story => ({ ...story, ...newStory, id: uuid() }));
+    setMadlib(
+      madlib => `The ${color} ${noun} adored the ${adjective} ${nounTwo}`
+    );
   };
 
   return (
     <div>
       <h1>Madlibs!</h1>
       <MadlibForm createStory={createStory} />
-      <h3>
-        The {color} {noun} adored the {adjective} {nounTwo}
-      </h3>
+      <h3>{madlib}</h3>
     </div>
   );
 };
